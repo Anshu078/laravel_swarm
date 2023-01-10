d2c334fcbf0a6677e7ee5da693caf6167bf1ef8e
anshu.maria <anshumaria0786@gmail.com>
2023-01-10 22:50:21 +0530

    welcome-blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LARAVEL#######
+                    LARAVEL-DOCKER-PIPELINE
                 </div>
 
                 <div class="links">
