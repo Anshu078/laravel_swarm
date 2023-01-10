6101f2e83a34238e03ade8e44164bec602de6f8d
anshu.maria <anshumaria0786@gmail.com>
2023-01-10 21:13:28 +0530

    welcome.blade file editing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                     Laravel-PIPELINE
+                     LARAVEL-PIPELINE
                 </div>
 
                 <div class="links">
