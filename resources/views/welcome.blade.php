fe8690b159e8dff4963f4edaaf552dec6355c6e2
anshu.maria <anshumaria0786@gmail.com>
2023-01-11 22:16:06 +0530

    welcome-blade fiel edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "Step 1 git 2 jenkins 3 Docker 4 Docker Registery 5 Ansible";
+echo "Step 1 git as code  2 jenkins as CI-CD 3 Docker as Containerization 4 DockerRegistery as Centralization 5 Ansible as Deployment Tool";
 ?>
 
 </body>
